dd39108ee672ff38e4a5f4cb7a27087b6b28e257
JesusLopezPob <trex555@comunidad.unam.mx>
2025-02-19 11:58:35 -0600

    PageDXL commit

--- codigos/DynaPaginaMult/DynaPaginaMult.ino
+++ codigos/DynaPaginaMult/DynaPaginaMult.ino
@@ -17,7 +17,7 @@ WiFiServer server(80);
 
 HardwareSerial& DXL_SERIAL = Serial2;
 Dynamixel2Arduino dxl(DXL_SERIAL, DXL_DIR_PIN);
-//This namespace is required to use Control table item names
+// Este namespace es necesario para usar los nombres de ítems de la tabla de control
 using namespace ControlTableItem;
 
 const int DXL_Baud = 1000000;
@@ -26,7 +26,7 @@ const int DXL_Baud = 1000000;
 // Para servos que usan protocolo 2.0 (Servo 1 y 2)
 // -------------------------
 const uint8_t SERVO_IDS[] = {1, 2};
-const int NUM_SERVOS = sizeof(SERVO_IDS);
+const int NUM_SERVOS = sizeof(SERVO_IDS) / sizeof(SERVO_IDS[0]);
 const float PROTOCOL_VERSION_2 = 2.0;
 const int MAX_UNIT_2 = 4095;
 const int MAX_ANGLE_2 = 360;
@@ -62,6 +62,13 @@ uint16_t servo3_speed     = 50;    // Velocidad de movimiento
 const uint8_t TURN_ON  = 1;
 const uint8_t TURN_OFF = 0;
 
+uint16_t position_p_gain[] = {120, 120};
+uint16_t position_i_gain[] = {15, 15};
+uint16_t position_d_gain[] = {1, 1};
+
+uint16_t velocity[] = {100, 100};   // Velocidad inicial
+uint16_t acceleration[] = {50, 50}; // Aceleración inicial
+
 // Tiempo de espera para las operaciones (ms)
 #define TIMEOUT 1000
 
@@ -74,10 +81,12 @@ String getValue(String data, String parameter) {
   startIndex += parameter.length() + 1;
   int endIndex = data.indexOf("&", startIndex);
   if (endIndex == -1) {
-    return data.substring(startIndex);
-  } else {
-    return data.substring(startIndex, endIndex);
+    endIndex = data.indexOf(" ", startIndex); // Buscar el espacio antes de "HTTP/1.1"
+    if (endIndex == -1) {
+      endIndex = data.length();
+    }
   }
+  return data.substring(startIndex, endIndex);
 }
 
 // =========================
@@ -96,6 +105,14 @@ void setup() {
     dxl.ping(id);
     dxl.torqueOff(id);
     dxl.setOperatingMode(id, OP_POSITION);
+
+    // Aplicar valores iniciales del PID y otros parámetros
+    dxl.writeControlTableItem(POSITION_P_GAIN, id, position_p_gain[i]);
+    dxl.writeControlTableItem(POSITION_I_GAIN, id, position_i_gain[i]);
+    dxl.writeControlTableItem(POSITION_D_GAIN, id, position_d_gain[i]);
+    dxl.writeControlTableItem(PROFILE_VELOCITY, id, velocity[i]);
+    dxl.writeControlTableItem(PROFILE_ACCELERATION, id, acceleration[i]);
+
     dxl.torqueOn(id);
     Serial.println("Servo " + String(id) + " configurado en posición (Protocolo 2.0).");
   }
@@ -104,7 +121,7 @@ void setup() {
   dxl.setPortProtocolVersion(PROTOCOL_VERSION_1);
   if (dxl.ping(SERVO3_ID)) {
     Serial.println("Servo 3 encontrado (Protocolo 1.0).");
-      // Apagar torque para configurar
+    // Apagar torque para configurar
     if (dxl.write(SERVO3_ID, TORQUE_ENABLE_ADDR, (uint8_t*)&TURN_OFF, TORQUE_ENABLE_ADDR_LEN, TIMEOUT)) {
       Serial.println("Servo 3: Torque apagado.");
     } else {
@@ -154,23 +171,25 @@ void loop() {
     String request = client.readStringUntil('\r');
     Serial.println("Solicitud: " + request);
 
-    // Procesa el control de posición (para los 3 servos)
+    // Extraemos el parámetro "tab" para saber en qué pestaña nos encontramos
+    String activeTab = getValue(request, "tab");
+    if (activeTab == "") activeTab = "pestana1";  // Valor por defecto
+
     if (request.indexOf("/set_position") != -1) {
+      // Extraer parámetros: id del servo, valor y tipo
       String servoStr = getValue(request, "servo");
       String valueStr = getValue(request, "value");
-      String typeStr  = getValue(request, "type");
+      String typeStr  = getValue(request, "type");  // "angle" o "unit"
       int servoID = servoStr.toInt();
-      int posValue = valueStr.toInt();
 
-      // Si es servo 3, se usa protocolo 1.0 y sus direcciones
+      // Para el servo 3 (protocolo 1.0) se hace conversión manual
       if (servoID == SERVO3_ID) {
-        // Para servo 3 asumimos:
-        // - Si se usa "angle", se mapea de 0 a MAX_ANGLE_1 (por ejemplo, 0-300°) al rango interno 0-MAX_UNIT_1.
+        int posValue = valueStr.toInt();
         if (typeStr == "angle") {
           posValue = map(posValue, 0, MAX_ANGLE_1, 0, MAX_UNIT_1);
         }
         posValue = constrain(posValue, 0, MAX_UNIT_1);
-        // Cambiar a protocolo 1.0 para enviar el comando
+
         dxl.setPortProtocolVersion(PROTOCOL_VERSION_1);
         uint16_t pos = posValue;
         if (dxl.write(SERVO3_ID, GOAL_POSITION_ADDR, (uint8_t*)&pos, GOAL_POSITION_ADDR_LEN, TIMEOUT)) {
@@ -178,17 +197,19 @@ void loop() {
         } else {
           Serial.println("Error moviendo el servo 3.");
         }
-        // Regresar a protocolo 2.0 para los demás servos
         dxl.setPortProtocolVersion(PROTOCOL_VERSION_2);
       }
-      // Para servo 1 y 2 (protocolo 2.0)
+      // Para servos 1 y 2 (protocolo 2.0)
       else {
         if (typeStr == "angle") {
-          posValue = map(posValue, 0, MAX_ANGLE_2, 0, MAX_UNIT_2);
+          float angle = valueStr.toFloat();
+          dxl.setGoalPosition(servoID, angle, UNIT_DEGREE);
+          Serial.println("Servo " + String(servoID) + " posicionado a: " + String(angle) + "°");
+        } else {
+          int posValue = valueStr.toInt();
+          dxl.setGoalPosition(servoID, posValue);
+          Serial.println("Servo " + String(servoID) + " posicionado a: " + String(posValue) + " unidades");
         }
-        posValue = constrain(posValue, 0, MAX_UNIT_2);
-        dxl.setGoalPosition(servoID, posValue);
-        Serial.println("Servo " + String(servoID) + " posicionado a: " + String(posValue));
       }
     }
 
@@ -205,7 +226,8 @@ void loop() {
         dxl.writeControlTableItem(POSITION_P_GAIN, servoID, pVal);
         dxl.writeControlTableItem(POSITION_I_GAIN, servoID, iVal);
         dxl.writeControlTableItem(POSITION_D_GAIN, servoID, dVal);
-        Serial.println("Servo " + String(servoID) + " PID -> P:" + String(pVal) + " I:" + String(iVal) + " D:" + String(dVal));
+        Serial.println("Servo " + String(servoID) + " PID -> P:" + String(pVal) +
+                       " I:" + String(iVal) + " D:" + String(dVal));
       }
     }
 
@@ -235,6 +257,19 @@ void loop() {
       }
     }
 
+    // Antes de enviar la página, leemos los parámetros actuales de los servos 1 y 2
+    uint16_t currentP1   = dxl.readControlTableItem(POSITION_P_GAIN, 1);
+    uint16_t currentI1   = dxl.readControlTableItem(POSITION_I_GAIN, 1);
+    uint16_t currentD1   = dxl.readControlTableItem(POSITION_D_GAIN, 1);
+    uint16_t currentVel1 = dxl.readControlTableItem(PROFILE_VELOCITY, 1);
+    uint16_t currentAcc1 = dxl.readControlTableItem(PROFILE_ACCELERATION, 1);
+    
+    uint16_t currentP2   = dxl.readControlTableItem(POSITION_P_GAIN, 2);
+    uint16_t currentI2   = dxl.readControlTableItem(POSITION_I_GAIN, 2);
+    uint16_t currentD2   = dxl.readControlTableItem(POSITION_D_GAIN, 2);
+    uint16_t currentVel2 = dxl.readControlTableItem(PROFILE_VELOCITY, 2);
+    uint16_t currentAcc2 = dxl.readControlTableItem(PROFILE_ACCELERATION, 2);
+
     // --- Enviar página HTML ---
     client.println("HTTP/1.1 200 OK");
     client.println("Content-Type: text/html");
@@ -256,18 +291,35 @@ void loop() {
     client.println("</head>");
     client.println("<body>");
     client.println("  <header><h1>Control de Dynamixel</h1></header>");
+
+    // Contenedor de pestañas
     client.println("  <div class=\"tab-container\">");
-    client.println("    <div class=\"tab active\" onclick=\"openTab(event, 'pestana1')\">Servo 1</div>");
-    client.println("    <div class=\"tab\" onclick=\"openTab(event, 'pestana2')\">Servo 2</div>");
-    client.println("    <div class=\"tab\" onclick=\"openTab(event, 'pestana3')\">Servo 3</div>");
+    if (activeTab.equals("pestana1"))
+      client.println("    <div class=\"tab active\" onclick=\"openTab(event, 'pestana1')\">Servo 1</div>");
+    else
+      client.println("    <div class=\"tab\" onclick=\"openTab(event, 'pestana1')\">Servo 1</div>");
+    
+    if (activeTab.equals("pestana2"))
+      client.println("    <div class=\"tab active\" onclick=\"openTab(event, 'pestana2')\">Servo 2</div>");
+    else
+      client.println("    <div class=\"tab\" onclick=\"openTab(event, 'pestana2')\">Servo 2</div>");
+    
+    if (activeTab.equals("pestana3"))
+      client.println("    <div class=\"tab active\" onclick=\"openTab(event, 'pestana3')\">Servo 3</div>");
+    else
+      client.println("    <div class=\"tab\" onclick=\"openTab(event, 'pestana3')\">Servo 3</div>");
     client.println("  </div>");
 
     // Pestaña Servo 1
-    client.println("  <div id=\"pestana1\" class=\"tab-content active\">");
+    if (activeTab.equals("pestana1"))
+      client.println("  <div id=\"pestana1\" class=\"tab-content active\">");
+    else
+      client.println("  <div id=\"pestana1\" class=\"tab-content\">");
     client.println("    <h2>Control del Servo 1</h2>");
     client.println("    <h3>Control de Posición</h3>");
     client.println("    <form action=\"/set_position\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"1\">");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana1\">");
     client.println("      <label>Posición (0-4095 o 0-360 si es 'angle'):</label><br>");
     client.println("      <input type=\"number\" name=\"value\" min=\"0\" max=\"4095\" required><br>");
     client.println("      <input type=\"radio\" name=\"type\" value=\"unit\" checked> Unidad interna<br>");
@@ -277,31 +329,38 @@ void loop() {
     client.println("    <h3>Ajustar PID</h3>");
     client.println("    <form action=\"/set_pid\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"1\">");
-    client.println("      <label>P:</label><input type=\"number\" name=\"p\" min=\"0\" max=\"32767\" required><br>");
-    client.println("      <label>I:</label><input type=\"number\" name=\"i\" min=\"0\" max=\"32767\" required><br>");
-    client.println("      <label>D:</label><input type=\"number\" name=\"d\" min=\"0\" max=\"32767\" required><br>");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana1\">");
+    client.println("      <label>P:</label><input type=\"number\" name=\"p\" min=\"0\" max=\"32767\" value=\"" + String(currentP1) + "\" required><br>");
+    client.println("      <label>I:</label><input type=\"number\" name=\"i\" min=\"0\" max=\"32767\" value=\"" + String(currentI1) + "\" required><br>");
+    client.println("      <label>D:</label><input type=\"number\" name=\"d\" min=\"0\" max=\"32767\" value=\"" + String(currentD1) + "\" required><br>");
     client.println("      <button type=\"submit\">Actualizar PID</button>");
     client.println("    </form>");
     client.println("    <h3>Ajustar Velocidad</h3>");
     client.println("    <form action=\"/set_velocity\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"1\">");
-    client.println("      <label>Velocidad:</label><input type=\"number\" name=\"v\" min=\"0\" max=\"1023\" required><br>");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana1\">");
+    client.println("      <label>Velocidad:</label><input type=\"number\" name=\"v\" min=\"0\" max=\"1023\" value=\"" + String(currentVel1) + "\" required><br>");
     client.println("      <button type=\"submit\">Actualizar Velocidad</button>");
     client.println("    </form>");
     client.println("    <h3>Ajustar Aceleración</h3>");
     client.println("    <form action=\"/set_acceleration\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"1\">");
-    client.println("      <label>Aceleración:</label><input type=\"number\" name=\"a\" min=\"0\" max=\"1023\" required><br>");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana1\">");
+    client.println("      <label>Aceleración:</label><input type=\"number\" name=\"a\" min=\"0\" max=\"1023\" value=\"" + String(currentAcc1) + "\" required><br>");
     client.println("      <button type=\"submit\">Actualizar Aceleración</button>");
     client.println("    </form>");
     client.println("  </div>");
 
     // Pestaña Servo 2
-    client.println("  <div id=\"pestana2\" class=\"tab-content\">");
+    if (activeTab.equals("pestana2"))
+      client.println("  <div id=\"pestana2\" class=\"tab-content active\">");
+    else
+      client.println("  <div id=\"pestana2\" class=\"tab-content\">");
     client.println("    <h2>Control del Servo 2</h2>");
     client.println("    <h3>Control de Posición</h3>");
     client.println("    <form action=\"/set_position\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"2\">");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana2\">");
     client.println("      <label>Posición (0-4095 o 0-360 si es 'angle'):</label><br>");
     client.println("      <input type=\"number\" name=\"value\" min=\"0\" max=\"4095\" required><br>");
     client.println("      <input type=\"radio\" name=\"type\" value=\"unit\" checked> Unidad interna<br>");
@@ -311,31 +370,38 @@ void loop() {
     client.println("    <h3>Ajustar PID</h3>");
     client.println("    <form action=\"/set_pid\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"2\">");
-    client.println("      <label>P:</label><input type=\"number\" name=\"p\" min=\"0\" max=\"32767\" required><br>");
-    client.println("      <label>I:</label><input type=\"number\" name=\"i\" min=\"0\" max=\"32767\" required><br>");
-    client.println("      <label>D:</label><input type=\"number\" name=\"d\" min=\"0\" max=\"32767\" required><br>");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana2\">");
+    client.println("      <label>P:</label><input type=\"number\" name=\"p\" min=\"0\" max=\"32767\" value=\"" + String(currentP2) + "\" required><br>");
+    client.println("      <label>I:</label><input type=\"number\" name=\"i\" min=\"0\" max=\"32767\" value=\"" + String(currentI2) + "\" required><br>");
+    client.println("      <label>D:</label><input type=\"number\" name=\"d\" min=\"0\" max=\"32767\" value=\"" + String(currentD2) + "\" required><br>");
     client.println("      <button type=\"submit\">Actualizar PID</button>");
     client.println("    </form>");
     client.println("    <h3>Ajustar Velocidad</h3>");
     client.println("    <form action=\"/set_velocity\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"2\">");
-    client.println("      <label>Velocidad:</label><input type=\"number\" name=\"v\" min=\"0\" max=\"1023\" required><br>");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana2\">");
+    client.println("      <label>Velocidad:</label><input type=\"number\" name=\"v\" min=\"0\" max=\"1023\" value=\"" + String(currentVel2) + "\" required><br>");
     client.println("      <button type=\"submit\">Actualizar Velocidad</button>");
     client.println("    </form>");
     client.println("    <h3>Ajustar Aceleración</h3>");
     client.println("    <form action=\"/set_acceleration\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"2\">");
-    client.println("      <label>Aceleración:</label><input type=\"number\" name=\"a\" min=\"0\" max=\"1023\" required><br>");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana2\">");
+    client.println("      <label>Aceleración:</label><input type=\"number\" name=\"a\" min=\"0\" max=\"1023\" value=\"" + String(currentAcc2) + "\" required><br>");
     client.println("      <button type=\"submit\">Actualizar Aceleración</button>");
     client.println("    </form>");
     client.println("  </div>");
 
     // Pestaña Servo 3 (solo posición)
-    client.println("  <div id=\"pestana3\" class=\"tab-content\">");
+    if (activeTab.equals("pestana3"))
+      client.println("  <div id=\"pestana3\" class=\"tab-content active\">");
+    else
+      client.println("  <div id=\"pestana3\" class=\"tab-content\">");
     client.println("    <h2>Control del Servo 3 (Solo Posición)</h2>");
     client.println("    <h3>Control de Posición</h3>");
     client.println("    <form action=\"/set_position\" method=\"get\">");
     client.println("      <input type=\"hidden\" name=\"servo\" value=\"3\">");
+    client.println("      <input type=\"hidden\" name=\"tab\" value=\"pestana3\">");
     client.println("      <label>Posición (0-1023 o 0-300 si es 'angle'):</label><br>");
     client.println("      <input type=\"number\" name=\"value\" min=\"0\" max=\"1023\" required><br>");
     client.println("      <input type=\"radio\" name=\"type\" value=\"unit\" checked> Unidad interna<br>");
